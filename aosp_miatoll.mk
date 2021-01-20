178d427829d1f3bd9776680077f485bfc07e82b5
Mikdadali <mikdad.momin121@gmail.com>
2021-01-20 10:21:01 +0000

    pex bringup

---
+++ aosp_miatoll.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2020 The Project-Xtended
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from miatoll  device
+$(call inherit-product, device/xiaomi/miatoll/device.mk)
+
+# Inherit some common xtended stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Bootanimation Resolution
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Device identifier
+PRODUCT_NAME := aosp_miatoll
+PRODUCT_DEVICE := miatoll
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := SM6250
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
